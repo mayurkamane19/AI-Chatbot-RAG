9b12794372e5dd71cbd51d4f9018af50b6b0ddca
mayurkamane19 <mayurkamane19@gmail.com>
2026-06-23 11:45:40 +0530

    Fix CORS issue

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.rag import create_vector_db, ask_question
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
